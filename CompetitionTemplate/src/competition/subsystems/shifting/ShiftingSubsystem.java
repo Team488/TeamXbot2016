7fc7044649b0809de748fa372f5fbc22c1b323a4
tfrancisco <tfrancisco@forestridge.org>
2016-02-04 20:48:56 -0800

    Got rid of the OI

--- CompetitionTemplate/src/competition/subsystems/shifting/ShiftingSubsystem.java
+++ CompetitionTemplate/src/competition/subsystems/shifting/ShiftingSubsystem.java
@@ -3,13 +3,12 @@ package competition.subsystems.shifting;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XSolenoid;
 import xbot.common.injection.wpi_factories.WPIFactory;
-import competition.operator_interface.OperatorInterface;
 
 
 public class ShiftingSubsystem extends BaseSubsystem {
     public final XSolenoid solenoid;
     
-    public ShiftingSubsystem(WPIFactory factory, OperatorInterface oi){
+    public ShiftingSubsystem(WPIFactory factory){
         solenoid = factory.getSolenoid(1);
     }
     
