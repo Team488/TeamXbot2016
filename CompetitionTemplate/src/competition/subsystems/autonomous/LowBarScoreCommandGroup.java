c78acccd60dd4a1ff6f518adf157e14a941a13b9
tfrancisco <tfrancisco@forestridge.org>
2016-02-20 13:15:06 -0800

    Low Goal Low BAsr;aidjf

--- CompetitionTemplate/src/competition/subsystems/autonomous/LowBarScoreCommandGroup.java
+++ CompetitionTemplate/src/competition/subsystems/autonomous/LowBarScoreCommandGroup.java
@@ -3,6 +3,8 @@ package competition.subsystems.autonomous;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import competition.subsystems.collector.CollectorSubsystem;
+import competition.subsystems.collector.commands.CollectorEjectCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.PoseSubsystem;
 import competition.subsystems.drive.commands.HeadingModule;
@@ -15,26 +17,30 @@ public class LowBarScoreCommandGroup extends CommandGroup{
     public DoubleProperty distanceToTurningPoint;
     public DoubleProperty distanceToLowGoal;
     DoubleProperty targetHeading;
+    DoubleProperty ballEjectTime;
     
     @Inject
-    public LowBarScoreCommandGroup (DriveSubsystem driveSubsystem, HeadingModule headingModule, PoseSubsystem poseSubsystem, XPropertyManager propManager){
-        distanceToTurningPoint = propManager.createPersistentProperty("distance to turning point", 147.7);
-        DriveForDistanceCommand driveToTurningPoint = new DriveForDistanceCommand(poseSubsystem, driveSubsystem, propManager);
+    public LowBarScoreCommandGroup (DriveSubsystem drive, HeadingModule headingModule, PoseSubsystem pose, CollectorSubsystem collector, XPropertyManager propManager){
+        distanceToTurningPoint = propManager.createPersistentProperty("distance to turning point", -147.7);
+        DriveForDistanceCommand driveToTurningPoint = new DriveForDistanceCommand(pose, drive, propManager);
         driveToTurningPoint.setTargetDistance(distanceToTurningPoint.get());
         
         this.addSequential(driveToTurningPoint);
         
         targetHeading = propManager.createPersistentProperty("Target heading to turn to lowGoal", 240.0);
-        TurnToHeadingCommand turnToLowGoal = new TurnToHeadingCommand(driveSubsystem, headingModule, poseSubsystem, propManager);
+        TurnToHeadingCommand turnToLowGoal = new TurnToHeadingCommand(drive, headingModule, pose, propManager);
         turnToLowGoal.setTargetHeading(targetHeading.get());
         
         this.addSequential(turnToLowGoal);
         
         distanceToLowGoal = propManager.createPersistentProperty("distance to low goal", 139.03);
-        DriveForDistanceCommand driveToLowGoal = new DriveForDistanceCommand(poseSubsystem, driveSubsystem, propManager);
+        DriveForDistanceCommand driveToLowGoal = new DriveForDistanceCommand(pose, drive, propManager);
         driveToLowGoal.setTargetDistance(distanceToLowGoal.get());
         
         this.addSequential(driveToLowGoal);
+        
+        CollectorEjectCommand collectorEjectCommand = new CollectorEjectCommand(collector, propManager);
+        this.addSequential(collectorEjectCommand);
         
     }
 
