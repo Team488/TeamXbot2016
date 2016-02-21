699ee000e071f625d6eff00bd67786c21504ac81
John Gilbert <johngilb@gmail.com>
2016-02-20 16:19:38 -0800

    Add second wrist solenoid and disable auto arm calibration

--- CompetitionTemplate/src/competition/subsystems/wrist/WristSubsystem.java
+++ CompetitionTemplate/src/competition/subsystems/wrist/WristSubsystem.java
@@ -1,7 +1,5 @@
 package competition.subsystems.wrist;
 
-import org.apache.log4j.Logger;
-
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import competition.subsystems.drive.DriveSubsystem;
@@ -12,22 +10,26 @@ import xbot.common.injection.wpi_factories.WPIFactory;
 
 @Singleton
 public class WristSubsystem extends BaseSubsystem {
-    public final XSolenoid solenoid;
+    public final XSolenoid solenoidA;
+    public final XSolenoid solenoidB;
     
     private static Logger log = Logger.getLogger(WristSubsystem.class);
     
     @Inject
     public WristSubsystem (WPIFactory factory) {
-        log.info("Creating WristSubsystem");
-        solenoid = factory.getSolenoid(3);
+    	log.info("Creating WristSubsystem");
+        solenoidA = factory.getSolenoid(2);
+        solenoidB = factory.getSolenoid(3);
     }
     
     public void moveWristUp() {
-        solenoid.set(true);
+        solenoidA.set(true);
+        solenoidB.set(false);
     }
     
     public void moveWristDown() {
-        solenoid.set(false);
+        solenoidA.set(false);
+        solenoidB.set(true);
     }
 
 }
