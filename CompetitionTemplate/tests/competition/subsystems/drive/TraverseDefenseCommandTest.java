3e15a091f6895fddd584ae93bf237549165b0157
Alex Schokking <aschokking@gmail.com>
2016-02-23 18:24:03 -0800

    Add better test

--- CompetitionTemplate/tests/competition/subsystems/drive/TraverseDefenseCommandTest.java
+++ CompetitionTemplate/tests/competition/subsystems/drive/TraverseDefenseCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import competition.subsystems.drive.commands.MonitorDefenseTraversalModule;
 import competition.subsystems.drive.commands.TraverseDefenseCommand;
 import edu.wpi.first.wpilibj.MockTimer;
 import xbot.common.injection.BaseWPITest;
@@ -12,6 +13,7 @@ import xbot.common.injection.BaseWPITest;
 public class TraverseDefenseCommandTest extends BaseWPITest {
     
     TraverseDefenseCommand command;
+    PoseSubsystem pose;
     
     MockTimer timer;
     
@@ -21,6 +23,7 @@ public class TraverseDefenseCommandTest extends BaseWPITest {
         
         this.command = this.injector.getInstance(TraverseDefenseCommand.class);
         this.timer = this.injector.getInstance(MockTimer.class);
+        this.pose = this.injector.getInstance(PoseSubsystem.class);
         
         this.command.setPower(0.5);
         this.command.setTimeLimits(1, 2);
@@ -31,8 +34,30 @@ public class TraverseDefenseCommandTest extends BaseWPITest {
         
         command.initialize();
         assertFalse(command.isFinished());
-        
+
         this.timer.setTimeInSeconds(2.5);
+        
+        assertTrue(command.isFinished());
+        
+    }
+    
+    @Test
+    public void testDetectsOffDefense() {
+        this.pose.setDefenseState(MonitorDefenseTraversalModule.DefenseState.NotOnDefense);
+        command.initialize();
+        command.execute();
+        
+        this.timer.setTimeInSeconds(0.5);
+        this.pose.setDefenseState(MonitorDefenseTraversalModule.DefenseState.OnDefense);
+        command.execute();
+        assertFalse(command.isFinished());
+
+        this.pose.setDefenseState(MonitorDefenseTraversalModule.DefenseState.NotOnDefense);
+        command.execute();
+        assertFalse(command.isFinished());
+        
+        this.timer.setTimeInSeconds(1.5);
+        command.execute();
         
         assertTrue(command.isFinished());
         
