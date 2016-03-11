27ed01c2bb3a9a9e39d08f46006945f6e9d2b478
John Gilbert <johngilb@gmail.com>
2016-03-10 20:19:00 -0800

    Have a command group that crosses the Cheval

--- CompetitionTemplate/src/competition/defenseCommands/ChevalCommandGroup.java
+++ CompetitionTemplate/src/competition/defenseCommands/ChevalCommandGroup.java
@@ -6,17 +6,37 @@ import competition.subsystems.arm.arm_commands.ArmToBottomCommand;
 import competition.subsystems.arm.arm_commands.SetArmToAngleCommand;
 import competition.subsystems.drive.commands.DriveToDistanceCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.XPropertyManager;
 
 public class ChevalCommandGroup extends CommandGroup{
 
+    DriveToDistanceCommand backUp;
+    ArmToBottomCommand dropArm;
+    DriveToDistanceCommand crossDefense;
+    
+    DoubleProperty chevalBackUpDistance;
+    DoubleProperty chevalCrossDefenseDistance;
+    
     @Inject
     public ChevalCommandGroup(
             DriveToDistanceCommand backUp,
             ArmToBottomCommand dropArm,
-            DriveToDistanceCommand crossDefense) {
+            DriveToDistanceCommand crossDefense,
+            XPropertyManager propMan) {
         
-        backUp.setTargetDistance(-6);
-        crossDefense.setTargetDistance(20);
+        this.backUp = backUp;
+        this.dropArm = dropArm;
+        this.crossDefense = crossDefense;
+        
+        chevalBackUpDistance = propMan.createPersistentProperty("ChevalBackUpDistance", -6.0);
+        chevalCrossDefenseDistance = propMan.createPersistentProperty("ChevalCrossDefenseDistance", 20.0);
+    }
+    
+    @Override
+    public void initialize() {
+        backUp.setTargetDistance(chevalBackUpDistance.get());
+        crossDefense.setTargetDistance(chevalCrossDefenseDistance.get());
         
         this.addSequential(backUp);
         this.addSequential(dropArm);
