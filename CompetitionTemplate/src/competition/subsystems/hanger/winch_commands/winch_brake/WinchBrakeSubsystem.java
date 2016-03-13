9123c4955030fbac0fdad850bc34370da4c5d6ea
tfrancisco <tfrancisco@forestridge.org>
2016-03-13 14:19:51 -0700

    winch brake solenoid assigned 1

--- CompetitionTemplate/src/competition/subsystems/hanger/winch_commands/winch_brake/WinchBrakeSubsystem.java
+++ CompetitionTemplate/src/competition/subsystems/hanger/winch_commands/winch_brake/WinchBrakeSubsystem.java
@@ -5,13 +5,15 @@ import com.google.inject.Singleton;
 
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XSolenoid;
+import xbot.common.injection.wpi_factories.WPIFactory;
 
 @Singleton
 public class WinchBrakeSubsystem extends BaseSubsystem{
     public XSolenoid winchBrake;
     
     @Inject
-    public WinchBrakeSubsystem(){
+    public WinchBrakeSubsystem(WPIFactory factory){
+        winchBrake = factory.getSolenoid(1);
     }
     
     public void openBrake(){
