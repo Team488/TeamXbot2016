da717ef759946bae3c766b22ec6db18df5a53864
Alex Schokking <aschokking@gmail.com>
2016-03-18 12:02:21 -0700

    Add another command that drives for a moment slowly at the end of traverse defense to make sure we really made it through with our bumpers.

--- CompetitionTemplate/src/competition/subsystems/autonomous/RaiseArmAndTraverseDefenseCommandGroup.java
+++ CompetitionTemplate/src/competition/subsystems/autonomous/RaiseArmAndTraverseDefenseCommandGroup.java
@@ -6,6 +6,7 @@ import competition.subsystems.arm.arm_commands.ArmAngleMaintainerCommand;
 import competition.subsystems.arm.arm_commands.SetArmToAngleCommand;
 import competition.subsystems.arm.arm_commands.WaitForArmCalibrationCommand;
 import competition.subsystems.drive.commands.CalibrateHeadingCommand;
+import competition.subsystems.drive.commands.HeadingDriveCommand;
 import competition.subsystems.drive.commands.TraverseDefenseCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import xbot.common.properties.DoubleProperty;
@@ -17,8 +18,10 @@ public class RaiseArmAndTraverseDefenseCommandGroup extends CommandGroup{
     TraverseDefenseCommand traverse;
     SetArmToAngleCommand setArm;
     CalibrateHeadingCommand calibrateHeading;
+    HeadingDriveCommand moveToClearDefense;
     
     DoubleProperty moveFirstDuration;
+    DoubleProperty moveToClearDefenseDuration;
     
     public String label;
     
@@ -29,20 +32,24 @@ public class RaiseArmAndTraverseDefenseCommandGroup extends CommandGroup{
             SetArmToAngleCommand setArm,
             TraverseDefenseCommand traverse,
             CalibrateHeadingCommand calibrateHeading,
+            HeadingDriveCommand moveToClearDefense,
             XPropertyManager propMan) {
         
         this.traverse = traverse;
         this.setArm = setArm;
         this.calibrateHeading = calibrateHeading;
         this.moveFirst = moveFirst;
+        this.moveToClearDefense = moveToClearDefense;
         
         moveFirstDuration = propMan.createPersistentProperty("MoveFirstDuration", 0.75);
+        moveToClearDefenseDuration = propMan.createPersistentProperty("RaiseArmAndTraverse MoveToClearDuration", 0.5);
         
         this.addSequential(this.calibrateHeading);
         this.addSequential(moveFirst);
         this.addSequential(waitForArmCalibration);
         this.addSequential(this.setArm);
         this.addSequential(this.traverse);
+        this.addSequential(this.moveToClearDefense, this.moveToClearDefenseDuration.get());
     }
     
     public void setTraversalProperties(double power, double heading, double minSeconds, double maxSeconds) {
@@ -53,12 +60,15 @@ public class RaiseArmAndTraverseDefenseCommandGroup extends CommandGroup{
         moveFirst.setTargetHeading(heading);
         moveFirst.setTimeLimits(moveFirstDuration.get(), moveFirstDuration.get() + 0.05);
         
+        moveToClearDefense.setTargetHeading(heading);
+        
         double moveFirstPower = 0.4;
         if (power < 0) {
             moveFirstPower *= -1;
         }
         
         moveFirst.setPower(moveFirstPower);
+        moveToClearDefense.setPower(moveFirstPower);
     }
     
     public void setArmAngle(double goalAngle) {
